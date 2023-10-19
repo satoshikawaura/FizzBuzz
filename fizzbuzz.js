992b83029affc40a01ad560bdbc779af68898d10
satoshikawaura <135300637+satoshikawaura@users.noreply.github.com>
2023-10-19 14:22:02 +0900

    FizzBuzzCompleted

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,8 @@
 function fizzbuzz(n) {
-    if(n % 15 === 0){
+    if(typeof n === "string"){
+         n = parseInt(n)
+    }
+    if(n % 15 === 0 ){
       return 'FizzBuzz'
     }
     if(n % 3 === 0){
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -9,10 +9,18 @@ test('数値2を渡すと文字列2で返す', () => {
   expect(fizzbuzz(2)).toBe('2');
 })
 
+test('文字列2を渡すと文字列2で返す', () => {
+    expect(fizzbuzz("2")).toBe('2');
+  })
+
 test('数値3を渡すと文字列Fizzを返す',() => {
   expect(fizzbuzz(3)).toBe('Fizz');
 })
 
+test('文字列3を渡すと文字列Fizzを返す',() => {
+    expect(fizzbuzz("3")).toBe('Fizz');
+  })
+  
 test('数値6を渡すと文字列Fizzを返す',() => {
   expect(fizzbuzz(6)).toBe('Fizz');
 })
@@ -24,6 +32,10 @@ test('数値5を渡すと文字列Buzzを返す',() => {
 test('数値15を渡すと文字列FizzBuzzを返す',() => {
   expect(fizzbuzz(15)).toBe('FizzBuzz');
 })
+
+test('文字列15を渡すと文字列FizzBuzzを返す',() => {
+    expect(fizzbuzz("15")).toBe('FizzBuzz');
+  })
 
 test('数値10を渡すと文字列Buzzを返す',() => {
   expect(fizzbuzz(10)).toBe('Buzz');
